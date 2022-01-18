8fe6cc2e983eac8af7794ddee01e00db4284a673
AntonStruiGeneralSoft <a.struy@general-soft.by>
2022-01-18 15:11:54 +0300

    Добавлена возможность отправки email

--- app/Http/Controllers/API/RegisterController.php
+++ app/Http/Controllers/API/RegisterController.php
@@ -6,11 +6,17 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\API\RegisterRequest;
 use App\Http\Controllers\Controller;
+use Mail;
 
 class RegisterController extends Controller
 {
     public function __invoke(RegisterRequest $request) {
         $data = $request->validated();
-        return response()->json($data, 200);
+        Mail::send('mail', ['firstName' => $data['firstName'], 'lastName' => $data['lastName'], 'link' => ''], function ($message) {
+            $message->to($data['email'], $data['firstName'] . ' ' . $data['lastName']);
+            $message->subject('Registration');
+        });
+        
+        return response()->json();#$data, 200);
     }
 }
